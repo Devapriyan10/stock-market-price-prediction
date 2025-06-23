681a31ba5028fbb2fb3044269c28b646d4bbd28b
SamyakMishra072 <edithstandsfor@gmail.com>
2025-06-23 18:56:05 +0530

    FRESH

--- frontend/src/components/Chart.jsx
+++
@@ -1,7 +0,0 @@
-import { Line } from 'react-chartjs-2';
-
-export default function Chart({ data }) {
-  const labels = data.map(d => d.ds);
-  const vals = data.map(d => d.y);
-  return <Line data={{ labels, datasets: [{ label: 'Close', data: vals }] }} />;
-}
--- frontend/src/components/Form.jsx
+++
@@ -1,7 +0,0 @@
-export default function Form({ children, onSubmit }) {
-  return (
-    <form onSubmit={onSubmit} className="max-w-md mx-auto bg-white p-6 rounded shadow mt-8">
-      {children}
-    </form>
-  );
-}
